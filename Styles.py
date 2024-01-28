aac5062fb7935b224b38d6f62c5254acf1c6a0fe
Proxima282 <146644401+Proxima282@users.noreply.github.com>
2024-01-28 17:26:53 +0200

    Update Styles.py

--- Styles.py
+++ Styles.py
@@ -1,1 +1,13 @@
+style = """
 
+    QWidget{
+        background:rgb(0, 0, 0);
+    }
+    QListWidget{
+        border: 3px solid white;
+        color:white
+    }
+    QPushButton{
+        border:
+    }
+"""
