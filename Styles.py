eee958f6d0e2a1fa36009de08504a85398f14764
Proxima282 <146644401+Proxima282@users.noreply.github.com>
2024-02-04 16:47:51 +0200

    Update Styles.py

--- Styles.py
+++ Styles.py
@@ -1,13 +1,34 @@
 style = """
-
-    QWidget{
-        background:rgb(0, 0, 0);
+    QWidget {
+        background: #2200FF;
+        color: #111111;
+        border-color: #FF0000;
+    }
+    QPushButton {
+        border-style: none;
+        border-color: #050a0e;
+        border-width: 1px;
+        border-radius: 5px;
+        color: #FFFFFF;
+        padding: 2px;
+        background-color: #468ABF;
+    }
+    QPushButton:hover{
+        color: #111111;
+        border-width: 3px;
+        border-style: ridge;
+        border-color: #004579;
     }
     QListWidget{
-        border: 3px solid white;
-        color:white
+        border-style: solid;
+        border-width: 5px;
+        border-color: #FFFFFF;
+        color: #FFFFFF;
     }
-    QPushButton{
-        border:
+    QLabel{
+        border-style: solid;
+        border-width: 5px;
+        border-color: #FFFFFF;
+        color: #FFFFFF;
     }
 """
