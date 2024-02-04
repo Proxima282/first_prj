e1fc5eacaee865d0c1d8a079811e20ace876ef7e
Proxima282 <146644401+Proxima282@users.noreply.github.com>
2024-02-04 16:47:01 +0200

    Update PaintWindow.py

--- PaintWindow.py
+++ PaintWindow.py
@@ -1,46 +1,73 @@
-from PyQt5.QtWidgets import(
-    QMainWindow,QLabel,QVBoxLayout,QWidget,QFileDialog
-)
-from  PyQt5.QtGui import QMouseEvent, QPixmap, QPainter, QPen
-from PyQt5.QtCore import Qt
+
+from PyQt5.QtWidgets import QMainWindow, QLabel, QVBoxLayout, QWidget, QFileDialog
+from PyQt5.QtGui import QPixmap, QPainter, QPen
+from PyQt5.QtCore import *
 
 class PaintWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setGeometry(100, 100, 800,600)
+        # Ініціалізація користувацького інтерфейсу
+        self.initUI()
 
+    def initUI(self):
+        # Встановлення розміру та назви вікна
+        self.setGeometry(100, 100, 800, 600)
+        self.setWindowTitle('Paint Application')
+
+        # Створення мітки для відображення малюнка
         self.canvas = QLabel(self)
         self.canvas.setAlignment(Qt.AlignTop)
         self.canvas.setStyleSheet("border: 2px solid black;")
 
+        # Створення QPixmap для робочої області та встановлення білого фону
         self.image = QPixmap(self.size())
         self.image.fill(Qt.white)
         self.canvas.setPixmap(self.image)
 
-        #остання точка для малювання лінії
+        # Змінна для збереження останньої точки для малювання ліній
         self.last_point = None
 
+        # Встановлення макету та мітки як центрального віджета
         vbox = QVBoxLayout()
         vbox.addWidget(self.canvas)
 
-        contanier = QWidget()
-        contanier.setLayout(vbox)
-        self.setCentralWidget(contanier)
-    
-    def mousePressEvent(self,event):
+        container = QWidget()
+        container.setLayout(vbox)
+        self.setCentralWidget(container)
+
+        # Додати меню для збереження малюнка
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('File')
+
+        saveAction = fileMenu.addAction('Save')
+        saveAction.triggered.connect(self.saveImage)
+
+    def mousePressEvent(self, event):
+        # Обробка події натискання лівої кнопки миші
         if event.button() == Qt.LeftButton:
             self.last_point = event.pos()
-    
+
     def mouseMoveEvent(self, event):
-        if event.button() and Qt.LeftButton and self.last_point:
+        # Обробка події руху миші при утриманні лівої кнопки
+        if event.buttons() and Qt.LeftButton and self.last_point:
+            # Отримання об'єкта QPainter та малювання лінії
             painter = QPainter(self.image)
-            painter.setPen(QPen(Qt.black,2,Qt.Solidline))
-            painter.drawLine(self.last_point,event.pos())
+            painter.setPen(QPen(Qt.black, 2, Qt.SolidLine))
+            painter.drawLine(self.last_point, event.pos())
             painter.end()
             self.last_point = event.pos()
             self.canvas.setPixmap(self.image)
-    
+
     def mouseReleaseEvent(self, event):
+        # Обробка події відпускання лівої кнопки миші
         if event.button == Qt.LeftButton:
             self.last_point = None
+
+    def saveImage(self):
+        # Відкриття діалогового вікна для вибору місця та імені файлу
+        filePath, _ = QFileDialog.getSaveFileName(self, "Save Image", "", "PNG Files (*.png);;All Files (*)")
+
+        if filePath:
+            # Збереження малюнка у вибраний файл
+            self.image.save(filePath)
