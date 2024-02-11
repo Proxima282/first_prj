2860863065a3c205272395d7a3db87d8d258af76
Proxima282 <146644401+Proxima282@users.noreply.github.com>
2024-02-11 12:03:02 +0200

    Update Styles.py

--- Styles.py
+++ Styles.py
@@ -1,34 +1,35 @@
 style = """
     QWidget {
-        background: #2200FF;
-        color: #111111;
-        border-color: #e32727;
+        background: #b727e3;
+        color: #3927e3;
+        border-color: #e3276c;
     }
     QPushButton {
         border-style: none;
-        border-color: #050a0e;
+        border-color: #27e346;
         border-width: 1px;
         border-radius: 5px;
-        color: #eb6e34;
+        color: #27e36f;
         padding: 2px;
         background-color: #468ABF;
     }
     QPushButton:hover{
-        color: #111111;
+        color: #e3da27;
         border-width: 3px;
         border-style: ridge;
-        border-color: #004579;
+        border-color: #56e327;
     }
     QListWidget{
         border-style: solid;
         border-width: 5px;
-        border-color: #e3dd27;
+        border-color: #278ee3;
         color: #27e37b;
     }
     QLabel{
         border-style: solid;
         border-width: 5px;
-        border-color: #a727e3;
+        border-color: #e33d27;
         color: #27b4e3;
     }
 """
+
